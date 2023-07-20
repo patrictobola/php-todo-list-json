3943e5c688e9c7f8e3f2ce28291d4cf383ac44c0
patrictobola <tobola92@gmail.com>
2023-07-20 15:54:23 +0200

    fixed major bug in php. now todolist push obj

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -5,6 +5,16 @@ $data_json = file_get_contents($data_path);
 
 $tasks = json_decode($data_json);
 
+$new_task = $_POST['tasks'] ?? null;
+if ($new_task) {
+    $tasks[] = $new_task;
+    $json_task = json_encode($tasks);
+    file_put_contents($data_path, $json_task);
+    header('Content-Type: application/json');
+
+    echo $new_task;
+}
+
 header('Content-Type: application/json');
 
 echo json_encode($tasks);
