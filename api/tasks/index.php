fcd5b26c2b248f83e00d1cbd8d34b62152114305
patrictobola <tobola92@gmail.com>
2023-07-21 10:07:18 +0200

    minor fix

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -3,7 +3,7 @@ $data_path = __DIR__ . '/../../database/datas.json';
 
 $data_json = file_get_contents($data_path);
 
-$tasks = json_decode($data_json);
+$tasks = json_decode($data_json, true);
 
 $new_task = $_POST['tasks'] ?? null;
 if ($new_task) {
